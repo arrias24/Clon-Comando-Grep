fadee5941e35b4e382afbe927392808aef5bc214
arrias24 <andresarriasc@gmail.com>
2024-02-10 21:56:21 -0400

    AGREGADO: LIBRERIAS, ARGUMENTOS, VARIABLES, ARCHIVOS.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
-int main(){
+int main(int argc, char** args){
+
+    //VARIABLES
+
+    fstream archivo;
+    string texto[1200] = {};
+
+    //ABRIMOS EL ARCHIVO DE TEXTO DESDE EL ARGUMENTO
+
+    archivo.open(args[1]); 
+
+    //ASEGURAMOS DE QUE SE ABRIO CORRECTAMENTE
+
+    if (!archivo.is_open()) {
+        cout << "El archivo no se pudo iniciar" << endl; 
+        return 0;
+    }
 
     return 0;
 }
