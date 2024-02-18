ef3bc1b174b2cc88501e761201ba0ce1d1a5b1d7
arrias24 <andresarriasc@gmail.com>
2024-02-18 16:42:27 -0400

    feat: Agregada función de resaltado de palabras y contador de coincidencias

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,28 @@
 using namespace std;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-//FUNCION 
+//FUNCIONES 
 
 void transformarMinuscula(string &cadena){
     for(int i = 0; i < cadena.length(); i++){
         cadena[i] = tolower(cadena[i]);
     }
+}
+
+void resaltarPalabra(const string& palabra, const string& palabra_buscada, int color_texto, int& coincidencias) {
+    size_t posicion = palabra.find(palabra_buscada);
+    size_t inicio = 0;
+
+    while (posicion != string::npos) {
+        cout << palabra.substr(inicio, posicion - inicio);
+        SetConsoleTextAttribute(hConsole, color_texto);
+        cout << palabra.substr(posicion, palabra_buscada.length());
+        SetConsoleTextAttribute(hConsole, 7);
+        inicio = posicion + palabra_buscada.length();
+        posicion = palabra.find(palabra_buscada, inicio);
+        coincidencias++;
+    }
+    cout << palabra.substr(inicio) << " ";
 }
 
 
@@ -30,6 +46,7 @@ int main(int argc, char** args){
     
     string palabra_buscada = args[2];
     transformarMinuscula(palabra_buscada);
+    int coincidencias = 0;
 
     //COLOR PARA RESALTAR TEXTOS
 
